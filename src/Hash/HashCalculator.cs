6990d0d0a4d8ae9f0b629daddd27da985626e1b7
Hibi_10000 <87810571+Hibi-10000@users.noreply.github.com>
2024-08-23 01:15:12 +0900

    🔧 ImplicitUsingsを有効にする

--- src/Hash/HashCalculator.cs
+++ src/Hash/HashCalculator.cs
@@ -15,9 +15,6 @@
 // You should have received a copy of the GNU General Public License
 // along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-using System;
-using System.IO;
-using System.Windows.Forms;
 using Microsoft.Win32;
 
 namespace Hash
