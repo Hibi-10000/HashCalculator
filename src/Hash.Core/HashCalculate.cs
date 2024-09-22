38ff1815c4ebd676d921976e1ce2f7090aeaac0b
Hibi_10000 <87810571+Hibi-10000@users.noreply.github.com>
2024-09-22 23:00:48 +0900

    🎨 HashTypeの情報を一元化

--- src/Hash.Core/HashCalculate.cs
+++ src/Hash.Core/HashCalculate.cs
@@ -24,76 +24,54 @@ namespace Hash.Core
 {
     public static class HashCalculate
     {
-        private enum HashType
+        internal class HashType
         {
-            MD5,
-            SHA1,
-            SHA256,
-            SHA384,
-            SHA512,
-            [ShouldCheckSupport]
-            SHA3_256,
-            [ShouldCheckSupport]
-            SHA3_384,
-            [ShouldCheckSupport]
-            SHA3_512,
-            CRC16_IBM,
-            [Hidden]
-            CRC16_CCITT,
-            CRC32,
-            [Hidden]
-            CRC32C,
-            CRC64_ECMA,
-            [Hidden]
-            CRC64_ISO,
-            [Hidden]
-            RIPEMD160,
-        }
+            internal static readonly HashType[] Types = [
+                new("MD5"        , MD5     .Create()                                               ),
+                new("SHA1"       , SHA1    .Create()                                               ),
+                new("SHA256"     , SHA256  .Create()                                               ),
+                new("SHA384"     , SHA384  .Create()                                               ),
+                new("SHA512"     , SHA512  .Create()                                               ),
+                new("SHA3-256"   , SHA3_256.Create()                  , SHA3_256.IsSupported       ),
+                new("SHA3-384"   , SHA3_384.Create()                  , SHA3_384.IsSupported       ),
+                new("SHA3-512"   , SHA3_512.Create()                  , SHA3_512.IsSupported       ),
+                new("CRC16-IBM"  , new CRC(CRC.Polynomial.CRC16_IBM  ), true                , true ),
+                new("CRC16-CCITT", new CRC(CRC.Polynomial.CRC16_CCITT)                             ),
+                new("CRC32"      , new CRC(CRC.Polynomial.CRC32      )                             ),
+                new("CRC32C"     , new CRC(CRC.Polynomial.CRC32C     ), true                , true ),
+                new("CRC64-ECMA" , new CRC(CRC.Polynomial.CRC64_ECMA )                             ),
+                new("CRC64-ISO"  , new CRC(CRC.Polynomial.CRC64_ISO  ), true                , true ),
+                new("RIPEMD160"  , new RIPEMD160Managed()             , true                , true ),
+            ];
 
-        [AttributeUsage(AttributeTargets.Field)]
-        private class HiddenAttribute : Attribute;
+            internal readonly string Name;
+            internal readonly HashAlgorithm Provider;
+            internal readonly bool Supported;
+            internal readonly bool Hidden;
 
-        private static bool IsHidden(HashType hashType)
-        {
-            return hashType switch
+            private HashType(string name, HashAlgorithm provider, bool supported = true, bool hidden = false)
             {
-                HashType.CRC16_CCITT
-                    or HashType.CRC32C
-                    or HashType.CRC64_ISO
-                    or HashType.RIPEMD160
-                    => true,
-                _ => false
-            };
-        }
-
-        [AttributeUsage(AttributeTargets.Field)]
-        private class ShouldCheckSupportAttribute : Attribute;
-
-        private static bool IsSupported(HashType hashType)
-        {
-            return hashType switch
-            {
-                HashType.SHA3_256 => SHA3_256.IsSupported,
-                HashType.SHA3_384 => SHA3_384.IsSupported,
-                HashType.SHA3_512 => SHA3_512.IsSupported,
-                _ => true
-            };
+                Name = name;
+                Provider = provider;
+                Supported = supported;
+                Hidden = hidden;
+            }
         }
 
         public static string[] GetHashTypeNames(bool includeHidden = false)
         {
             return (
-                from type in Enum.GetValues<HashType>()
-                where IsSupported(type) && (includeHidden || !IsHidden(type))
-                select type.ToString().Replace("_", "-")
+                from type in HashType.Types
+                where type.Supported && (includeHidden || !type.Hidden)
+                select type.Name
             ).ToArray();
         }
 
         public static string? GetHash(string hashType, string filePath, bool upper, bool hyphen)
         {
-            foreach (HashType type in Enum.GetValues<HashType>())
+            foreach (HashType type in HashType.Types)
             {
-                if (hashType.Replace("-", "_") == type.ToString())
+                if (hashType == type.Name)
                 {
                     return GetHash(type, filePath, upper, hyphen);
                 }
@@ -103,29 +81,9 @@ namespace Hash.Core
 
         private static string GetHash(HashType hashType, string filePath, bool upper, bool hyphen)
         {
-            HashAlgorithm hashProvider = hashType switch
-            {
-                HashType.MD5          => MD5   .Create(),
-                HashType.SHA1         => SHA1  .Create(),
-                HashType.SHA256       => SHA256.Create(),
-                HashType.SHA384       => SHA384.Create(),
-                HashType.SHA512       => SHA512.Create(),
-                HashType.SHA3_256     => SHA3_256.Create(),
-                HashType.SHA3_384     => SHA3_384.Create(),
-                HashType.SHA3_512     => SHA3_512.Create(),
-                HashType.CRC16_IBM    => new CRC(CRC.Polynomial.CRC16_IBM  ),
-                HashType.CRC16_CCITT  => new CRC(CRC.Polynomial.CRC16_CCITT),
-                HashType.CRC32        => new CRC(CRC.Polynomial.CRC32      ),
-                HashType.CRC32C       => new CRC(CRC.Polynomial.CRC32C     ),
-                HashType.CRC64_ECMA   => new CRC(CRC.Polynomial.CRC64_ECMA ),
-                HashType.CRC64_ISO    => new CRC(CRC.Polynomial.CRC64_ISO  ),
-                HashType.RIPEMD160    => new RIPEMD160Managed(),
-                _ => throw new ArgumentOutOfRangeException(nameof(hashType), hashType, null),
-            };
-
             using FileStream fs = new FileStream(filePath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
             //using MemoryStream fs = new MemoryStream(System.Text.Encoding.ASCII.GetBytes("123456789"));
-            byte[] bs = hashProvider.ComputeHash(fs);
+            byte[] bs = hashType.Provider.ComputeHash(fs);
 
             string returnStr = BitConverter.ToString(bs);
             returnStr = upper ? returnStr.ToUpper() : returnStr.ToLower();
