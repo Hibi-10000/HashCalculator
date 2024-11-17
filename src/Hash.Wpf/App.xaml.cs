01d46ba76e8baa8eda741d30d3027add6697b41a
Hibi_10000 <87810571+Hibi-10000@users.noreply.github.com>
2024-11-17 22:07:36 +0900

    🚧 Program.csを移植

--- src/Hash.Wpf/App.xaml.cs
+++ src/Hash.Wpf/App.xaml.cs
@@ -16,7 +16,12 @@
 // along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
 using System.Windows;
+using Hash.Core;
+using Microsoft.Win32;
 
 namespace Hash.Wpf;
 
@@ -25,13 +30,75 @@ namespace Hash.Wpf;
 /// </summary>
 public partial class App : Application
 {
+    internal const string Major = "0";
+    internal const string Minor = "6";
+    internal const string Build = "2";
+    internal const string Ch = "";
+
+    internal const string SemVer = $"{Major}.{Minor}.{Build}";
+
     /// <summary>
     /// Application Entry Point.
     /// </summary>
     [STAThread]
     public static void Main() {
+        string[] args = Environment.GetCommandLineArgs();
+
+        foreach (string arg in args)
+        {
+            switch (arg)
+            {
+                case "/rc" when Environment.IsPrivilegedProcess:
+                    if (!File.Exists(Environment.ProcessPath))
+                    {
+                        MessageBox.Show("予期せぬ原因によりファイルパスを取得できませんでした。", "エラー", MessageBoxButton.OK, MessageBoxImage.Error);
+                        Environment.ExitCode = 1;
+                        return;
+                    }
+                    using (RegistryKey regKey = Registry.ClassesRoot.CreateSubKey(@"*\shell\HashForContext"))
+                    {
+                        regKey.SetValue("MUIVerb", "Hash for ContextMenu(&F)", RegistryValueKind.String);
+                        regKey.SetValue("SubCommands", "", RegistryValueKind.String);
+                    }
+                    using (RegistryKey regKey = Registry.ClassesRoot.CreateSubKey(@"*\shell\HashForContext\shell\*\command")) {
+                        regKey.SetValue("", @$"""{Environment.ProcessPath}"" /ctm /f ""%1""", RegistryValueKind.String);
+                    }
+
+                    foreach (string hash in HashCalculate.GetHashTypeNames())
+                    {
+                        using RegistryKey regKey = Registry.ClassesRoot.CreateSubKey(@$"*\shell\HashForContext\shell\{hash}\command");
+                        regKey.SetValue("", @$"""{Environment.ProcessPath}"" /ctm /f ""%1"" /h {hash}", RegistryValueKind.String);
+                    }
+                    return;
+                case "/rd" when Environment.IsPrivilegedProcess:
+                    Registry.ClassesRoot.DeleteSubKeyTree(@"*\shell\HashForContext");
+                    return;
+                case "/rd" or "/rc":
+                    MessageBox.Show("UACをキャンセルしたか、起動方法が間違っています。", "エラー", MessageBoxButton.OK, MessageBoxImage.Error);
+                    Environment.ExitCode = 1;
+                    return;
+                case "/ctm":
+                    //ApplicationConfiguration.Initialize();
+                    //Application.Run(new HashForContext());
+                    return;
+            }
+        }
+
+        const string mutexName = "HashCalculator-Mutex";
+        using Mutex mutex = new Mutex(true, mutexName, out bool createdNew);
+
         App app = new App();
         app.InitializeComponent();
         app.Run();
+        mutex.ReleaseMutex();
+    }
+
+    internal static void OpenLink(string link)
+    {
+        ProcessStartInfo startInfo = new ProcessStartInfo(link)
+        {
+            UseShellExecute = true
+        };
+        Process.Start(startInfo);
     }
 }
