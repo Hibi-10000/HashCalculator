5e5512652d940369f739f9f7484279353b335ff8
Hibi_10000 <87810571+Hibi-10000@users.noreply.github.com>
2024-11-21 01:19:05 +0900

    🚧 HashCalculator.csの移植 (Part 1)

--- src/Hash.Wpf/MainWindow.xaml.cs
+++ src/Hash.Wpf/MainWindow.xaml.cs
@@ -1,4 +1,4 @@
-﻿// Copyright © 2021-2024 Hibi_10000
+// Copyright © 2021-2024 Hibi_10000
 // 
 // This file is part of HashCalculator program.
 // 
@@ -15,7 +15,9 @@
 // You should have received a copy of the GNU General Public License
 // along with this program. If not, see <http://www.gnu.org/licenses/>.
 
+using System;
 using System.Windows;
+using Microsoft.Win32;
 
 namespace Hash.Wpf;
 
@@ -24,8 +26,56 @@ namespace Hash.Wpf;
 /// </summary>
 public partial class MainWindow : Window
 {
+    //[DllImport("UXTheme.dll", SetLastError = true, EntryPoint = "#138")]
+    //public static extern bool ShouldSystemUseDarkMode();
+
     public MainWindow(bool multiInstance)
     {
         InitializeComponent();
+        if (multiInstance)
+        {
+            HashForContextEnable.IsEnabled = false;
+            HashForContextEnableText.Text += " (最初のインスタンスでのみ変更できます)";
+        }
+    }
+
+    private void MainWindow_OnLoaded(object sender, RoutedEventArgs e)
+    {
+        string[] args = Environment.GetCommandLineArgs();
+        for (int i = 0; i < args.Length; i++)
+        {
+            switch (args[i])
+            {
+                case "/f":
+                    //File (/f "{FileURL}")
+                    int urlNum = i + 1;
+                    HashFileURL.Text = args[urlNum];
+                    break;
+                case "/h":
+                    //HashType (/h MD5)
+                    int hashTypeNum = i + 1;
+                    HashSelector.Text = args[hashTypeNum];
+                    break;
+                case "/d":
+                    //Debug (/d)
+                    //HashFileURL.ReadOnly = false;
+                    break;
+            }
+        }
+
+        //(ShouldSystemUseDarkMode() ? (Action)setDarkMode : setLightMode)();
+
+        const string regPath = @"*\shell\HashForContext";
+        try {
+            using RegistryKey? regKey = Registry.ClassesRoot.OpenSubKey(regPath);
+            HashForContextEnable.IsChecked = regKey != null;
+        } catch (Exception) {
+            HashForContextEnable.IsChecked = false;
+        }
+
+        Title = $"HashCalculator v{App.SemVer}{App.Ch}";
+        hashAndVer.Content = $"HashCalculator v{App.SemVer}";
+        HashVer.Content = $"HashCalculator v{App.SemVer}{App.Ch}";
+        CopyRight.Content = $"Copyright © 2021-{DateTime.Now.Year} Hibi__10000";
     }
 }
